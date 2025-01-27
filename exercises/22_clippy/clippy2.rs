fc4ee54f35ed4f2a90952fad0bafe8b544d803d5
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2025-01-27 17:30:58 -0500

    91/94

--- exercises/22_clippy/clippy2.rs
+++ exercises/22_clippy/clippy2.rs
@@ -1,9 +1,9 @@
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    // TODO: Fix the Clippy lint.
-    for x in option {
-        res += x;
+
+    if let Some(value) = option {
+        res += value;
     }
 
     println!("{res}");
